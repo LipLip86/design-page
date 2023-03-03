d7c313e3349d37686b7a899c7ecc68e48f8eef72
Sergey Kovalchuk <81366846+SergeyKovalchuk2086@users.noreply.github.com>
2023-03-03 20:05:20 +0700

    feature: fixed

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import photo from './Photomain.jpg'
 import s from './Banner.module.css';
 
-
 const Banner: any = () => {
   return (
     <div className={s.container}>
-        <img src={photo} />
+        <img className={s.container__image} src={photo} />
       <div className={s.text_block}>
-      <h1 className={s.header}>Let Your Home
-      Be Unique</h1>
-      <p>There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
-      <button>Get started</button>
+        <h1 className={s.header}>
+          Let Your Home Be Unique
+        </h1>
+        <p>
+          There are many variations of the passages of lorem Ipsum fromavailable, majority.
+        </p>
+        <button>
+          Get started
+        </button>
       </div>
-
     </div>
   )
-  }
+}
 
 export default Banner;
