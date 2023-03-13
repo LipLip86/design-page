a944e44bd4d12bbb55b9e6bc600b1d89e0b41160
Natalia Lipchanskaia <n.lipchanskaya@gmail.com>
2023-03-13 12:46:24 +0100

    Counter block added

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import s from "./counter.module.css"
+import s from "./counter.module.css";
+import line from "../../assets/Line.png";
 
 const Counter: any = () => {
   return (
     <div className={s.counter}>
-      <div className="counter1">
+      <div className={s.counter1}>
       <h3>12</h3>
       <p>Years of Experience</p>
+      <img src={ line } />
       </div>
-
+      <div className={s.counter1}>
+      <h3>85</h3>
+      <p>Success Projects</p>
+      <img src={ line } />
+      </div>
+      <div className={s.counter1}>
+      <h3>15</h3>
+      <p>Active Projects</p>
+      <img src={ line } />
+      </div>
+      <div className={s.counter1}>
+      <h3>95</h3>
+      <p>Happy Customers</p>
+      </div>
     </div>
 
   )
