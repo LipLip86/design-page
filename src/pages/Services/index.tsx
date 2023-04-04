3bf8172bf6dab1e329a6fd8ba9ec94b7ec1bc7ec
Natalia Lipchanskaia <n.lipchanskaya@gmail.com>
2023-04-04 12:57:56 +0200

    Join banner added

--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import s from './index.module.css';
 import ServiceBanner from "../../components/service_banner/ServiceBanner";
+import { Contacts } from "../../components";
 
 const Services = () => {
   return (
     <div>
       <ServiceBanner />
+      <Contacts />
     </div>
   )
 }
