9b808c25ac75c77cd84d12eed98561aee23a2932
Natalia Lipchanskaia <n.lipchanskaya@gmail.com>
2023-04-07 12:58:34 +0200

    Service blocks styled

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import s from "./styles.module.css";
 import img1 from "../../assets/Photo_work1.jpg";
 import icon1 from "../../assets/Icon_work1.svg";
+import img2 from "../../assets/Photo_work2.jpg";
+import icon2 from "../../assets/Icon_work2.svg";
 
 const Work: any = () => {
   return (
@@ -20,12 +22,36 @@ const Work: any = () => {
             <img src={icon1} alt="icon"/>
             <h2>01</h2>
             </div>
+            <div className={s.container_work_main__picture_block__right__middle}>
             <h3>Concept & Details</h3>
             <p>It is a long established fact will be distracted.
               Lorem Ipsum is simply dummy from text of the and
               typesetting indufstry.</p>
+              </div>
           </div>
         </div>
+        <div className={s.container_work_main__text}>
+      <h2>Idea for Work</h2>
+      <p>It is a long established fact will be distracted. Lorem Ipsum is simply dummy
+        text of the printing and typesetting industry.</p>
+        </div>
+        <div className={s.container_work_main__picture_block}>
+        <div className={s.container_work_main__picture_block__left}>
+          <img src={img2} alt="work" />
+          </div>
+          <div className={s.container_work_main__picture_block__right}>
+            <div className={s.container_work_main__picture_block__right__top}>
+            <img src={icon2} alt="icon"/>
+            <h2>02</h2>
+            </div>
+            </div>
+            {/* <div className={s.container_work_main__picture_block__right__middle}>
+            <h3>Concept & Details</h3>
+            <p>It is a long established fact will be distracted.
+              Lorem Ipsum is simply dummy from text of the and
+              typesetting indufstry.</p>
+              </div> */}
+    </div>
     </div>
   )
 }
