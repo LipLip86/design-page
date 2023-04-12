bb12777ec8afe7a916bb00e6ab3acbea2fe4582f
Natalia Lipchanskaia <n.lipchanskaya@gmail.com>
2023-04-12 12:16:59 +0200

    Blocks changed

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -4,6 +4,10 @@ import img1 from "../../assets/Photo_work1.jpg";
 import icon1 from "../../assets/Icon_work1.svg";
 import img2 from "../../assets/Photo_work2.jpg";
 import icon2 from "../../assets/Icon_work2.svg";
+import img3 from "../../assets/Photo_work3.jpg";
+import icon3 from "../../assets/Icon_work3.svg";
+import img4 from "../../assets/Photo_work4.jpg";
+import icon4 from "../../assets/Icon_work4.svg";
 
 const Work: any = () => {
   return (
@@ -34,12 +38,12 @@ const Work: any = () => {
 
       <div className={`${s.container_work_main__picture_block} ${s.custom}`}>
         <div className={s.container_work_main__picture_block__left}>
-          <img src={img1} alt="work" />
+          <img src={img2} alt="work" />
         </div>
         <div className={s.container_work_main__picture_block__right}>
           <div className={s.container_work_main__picture_block__right__top}>
-            <img src={icon1} alt="icon"/>
-            <h2>01</h2>
+            <img src={icon2} alt="icon"/>
+            <h2>02</h2>
           </div>
           <div className={s.container_work_main__picture_block__right__middle}>
             <h3>Concept & Details</h3>
@@ -52,12 +56,12 @@ const Work: any = () => {
 
       <div className={s.container_work_main__picture_block}>
         <div className={s.container_work_main__picture_block__left}>
-          <img src={img1} alt="work" />
+          <img src={img3} alt="work" />
         </div>
         <div className={s.container_work_main__picture_block__right}>
           <div className={s.container_work_main__picture_block__right__top}>
-            <img src={icon1} alt="icon"/>
-            <h2>01</h2>
+            <img src={icon3} alt="icon"/>
+            <h2>03</h2>
           </div>
           <div className={s.container_work_main__picture_block__right__middle}>
             <h3>Concept & Details</h3>
@@ -70,12 +74,12 @@ const Work: any = () => {
 
       <div className={`${s.container_work_main__picture_block} ${s.custom}`}>
         <div className={s.container_work_main__picture_block__left}>
-          <img src={img1} alt="work" />
+          <img src={img4} alt="work" />
         </div>
         <div className={s.container_work_main__picture_block__right}>
           <div className={s.container_work_main__picture_block__right__top}>
-            <img src={icon1} alt="icon"/>
-            <h2>01</h2>
+            <img src={icon4} alt="icon"/>
+            <h2>04</h2>
           </div>
           <div className={s.container_work_main__picture_block__right__middle}>
             <h3>Concept & Details</h3>
