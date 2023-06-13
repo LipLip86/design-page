4f04c71d3a07467ca283a0624d91800090d9fbd3
Natalia Lipchanskaia <n.lipchanskaya@gmail.com>
2023-06-13 18:24:06 +0200

    Navigation added

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import s from "./styles.module.css";
 import { BaseButton } from "../ui";
+import { NavLink } from "react-router-dom";
 
 const Contacts = (): JSX.Element => {
   return (
     <div className={s.contacts}>
       <h3 className={s.contacts__title}>Wanna join the interno?</h3>
       <p className={s.contacts__subtitle}>It is a long established fact will be distracted.</p>
-      <BaseButton variant="default" color="yellow" rightIcon={true}>
-        Contact With Us
-      </BaseButton>
+      <NavLink to="/contact" className={s.nav__list_link}>
+        <BaseButton variant="default" color="yellow" rightIcon={true}>
+          Contact With Us
+        </BaseButton>
+      </NavLink>
     </div>
   );
 };
